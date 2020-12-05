f50e4a6629c484ad298b11f496c2563734d00f09
knna <knna77@gmail.com>
2020-12-05 12:25:34 +0100

    exer1 acabat, tot organitzat

---
+++ js/01ex.js
@@ -0,0 +1,34 @@
+/*He declarat la variable promesa  baix peruqè demostra millor el 
+funcionament de la pràctica. El primer que s'executa és el últim que hi ha escrit
+*/
+var respPromesa;
+//retarda: retrasa el temps que li passem per paràmetre (ms)
+export function retarda(temps) {
+  /*Si es descomenta la segon linea s'executa reject.
+ per a demostrar que passa si la promessa no és compleix 
+ */
+  var dobleTemps = temps * 2;
+  //temps = temps * 2;
+  var promesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (temps == dobleTemps) {
+        reject('el tiempo no va bien ');
+      } else {
+        resolve('tiempo concluido ');
+      }
+    }, temps);
+  });
+  promesa
+    .then((res) => {
+      //quan es compleix la promesa
+      resposta.innerHTML = res;
+    })
+    .catch((error) => {
+      //quan no es compleix la promesa
+      resposta.innerHTML = error;
+    });
+}
+retarda(5000);
+var respPromesa;
+//referèmcia al div on mostrem la resposta
+respPromesa = elemento = document.getElementById('resposta');
